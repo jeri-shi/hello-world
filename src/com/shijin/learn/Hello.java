c9ff5ee185b7e18b17e5534a8794b47905da3309
jeri <shijin2008@gmail.com>
2017-01-31 15:49:03 +0800

    change output format

--- src/com/shijin/learn/Hello.java
+++ src/com/shijin/learn/Hello.java
@@ -14,7 +14,7 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		for (int j = 0; j < 10; j++) {
-			System.out.println("Hello World Again!" + (i + j));
+			System.out.println("Hello World Again!" + i + j);
 		}
 	}
 

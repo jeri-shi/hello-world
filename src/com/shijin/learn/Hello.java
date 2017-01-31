742afab468713438aec1a9c9ca75ec63c2477056
jeri <shijin2008@gmail.com>
2017-01-31 17:13:44 +0800

    Add a List

--- src/com/shijin/learn/Hello.java
+++ src/com/shijin/learn/Hello.java
@@ -4,22 +4,28 @@
 package com.shijin.learn;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
+ * This is the class to test Git
  * @author shijin
  *
  */
 public class Hello {
-	static int i = 50;
+	static int i = 0;
 	static SimpleDateFormat df = new SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
 		for (int j = 0; j < 10; j++) {
 			System.out.println("Hello World Again!" + i + j);
+			list.add("" + (i+j));
 		}
+		System.out.println(list.size());
 		System.out.println("Today is " + df.format(new Date(System.currentTimeMillis())));
 	}
 

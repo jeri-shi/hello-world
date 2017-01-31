98a09df91d7710c7905b0eb245d39171c89571bd
jeri <shijin2008@gmail.com>
2017-01-31 16:30:49 +0800

    Add DateTime

--- src/com/shijin/learn/Hello.java
+++ src/com/shijin/learn/Hello.java
@@ -3,12 +3,16 @@
  */
 package com.shijin.learn;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author shijin
  *
  */
 public class Hello {
-	static int i = 5;
+	static int i = 50;
+	static SimpleDateFormat df = new SimpleDateFormat("YYYY-MM-DD HH:mm:ss");
 	/**
 	 * @param args
 	 */
@@ -16,6 +20,7 @@ public class Hello {
 		for (int j = 0; j < 10; j++) {
 			System.out.println("Hello World Again!" + i + j);
 		}
+		System.out.println("Today is " + df.format(new Date(System.currentTimeMillis())));
 	}
 
 }
